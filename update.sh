36121d03b09844511e0975f9b01e14ffaf36da66
Beha <shacknetisp@mail.com>
2019-07-09 15:53:08 -0400

    update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
-set -ex
+set -e
+
+REPO="https://github.com/tigris-mt/aurum"
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 test -e update.sh
 
-rm -rf /tmp/aurum-release
-git clone --recursive "$HOME/.minetest/games/aurum" /tmp/aurum-release
+mkdir -p /tmp/aurum-release
+pushd /tmp/aurum-release
+if [ ! -d .git ]; then
+	git init
+	git remote add origin "$REPO"
+fi
+git remote set-url origin "$REPO"
+git fetch origin
+git reset --hard origin/"$BRANCH"
+git submodule update --init --recursive
+git clean -fxd
+popd
 
 git rm --cached -r .
 git add update.sh
 git clean -fxd
-find --not -name .git "$T"
+
+f() {
+	find /tmp/aurum-release -not -iwholename '*/.git*' "$@"
+}
+
+f -type d | while read n; do
+	echo $n
+done
