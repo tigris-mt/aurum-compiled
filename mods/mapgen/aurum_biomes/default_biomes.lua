1e9bef81815ff624112423e2467f3ae533370e7f
Beha <shacknetisp@mail.com>
2019-07-10 20:48:18 +0000

    Update: aurum master at 121e4e9

--- mods/mapgen/aurum_biomes/default_biomes.lua
+++
@@ -1,9 +0,0 @@
-aurum.biomes.register("aurum:aurum", {
-	name = "aurum_barrens",
-	node_top = "aurum_base:dirt",
-	depth_top = 2,
-	node_filler = "aurum_base:dirt",
-	depth_filler = 4,
-	heat_point = 50,
-	humidity_point = 50,
-})
