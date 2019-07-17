0b4a10726341b2f39d177485aa0bd050f639ad24
Beha <shacknetisp@mail.com>
2019-07-17 17:15:24 +0000

    Update: aurum master at d22ed73

---
+++ mods/items/aurum_equip/armor_enchants.lua
@@ -0,0 +1,32 @@
+local S = minetest.get_translator()
+
+aurum.tools.register_enchant_callback{
+	init = function(state, stack)
+		state.eqdef = gequip.get_eqdef(stack, true)
+	end,
+
+	apply = function(state, stack)
+		stack:get_meta():set_string("eqdef", minetest.serialize(state.eqdef))
+		return stack
+	end,
+}
+
+aurum.tools.register_enchant("speed", {
+	categories = {
+		boots = true,
+	},
+	description = S"Speed",
+	apply = function(state, level)
+		state.eqdef.effects.speed = state.eqdef.effects.speed * (1 + (level + 1) / 5)
+	end,
+})
+
+aurum.tools.register_enchant("psyche_shield", {
+	categories = {
+		helmet = true,
+	},
+	description = S"Psyche Shield",
+	apply = function(state, level)
+		state.eqdef.armor.psyche = state.eqdef.armor.psyche / (1 + level / 3)
+	end,
+})
