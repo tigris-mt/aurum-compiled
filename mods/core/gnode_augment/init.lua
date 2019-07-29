99972cf08c49407b0e5752247dfe7be5f152f475
Beha <shacknetisp@mail.com>
2019-07-29 02:55:32 +0000

    Update: aurum master at 019c9a6

---
+++ mods/core/gnode_augment/init.lua
@@ -0,0 +1,68 @@
+local M = {}
+gnode_augment = M
+
+M.players = {}
+
+local callbacks = {}
+
+function M.register_callback(f)
+	table.insert(callbacks, f)
+	return #callbacks
+end
+
+function M.remove_callback(idx)
+	table.remove(callbacks, idx)
+end
+
+local step_time = tonumber(minetest.settings:get("gnode_augment.step_time")) or 0.3
+local timer = 0
+minetest.register_globalstep(function(dtime)
+	timer = timer + dtime
+	if timer < step_time then
+		return
+	end
+	timer = 0
+
+	for _,player in ipairs(minetest.get_connected_players()) do
+		local name = player:get_player_name()
+		local prop = player:get_properties()
+		local data = M.players[name]
+		local pos = player:get_pos()
+
+		local stand_pos = vector.round(vector.add(pos, vector.new(0, -0.1, 0)))
+
+		data.nodes = {
+			top = minetest.get_node(vector.add(pos, vector.new(0, prop.eye_height, 0))),
+			bottom = minetest.get_node(vector.add(pos, vector.new(0, prop.stepheight / 2, 0))),
+			stand = minetest.get_node(stand_pos),
+		}
+
+		local stand_def = minetest.registered_nodes[data.nodes.stand.name]
+		if stand_def._on_standing then
+			stand_def._on_standing(stand_pos, data.nodes.stand, player)
+		end
+
+		for _,f in ipairs(callbacks) do
+			f(player, data)
+		end
+	end
+end)
+
+local ignore = {
+	name = "ignore",
+	param1 = 0,
+	param2 = 0,
+}
+minetest.register_on_joinplayer(function(player)
+	M.players[player:get_player_name()] = {
+		nodes = {
+			top = ignore,
+			bottom = ignore,
+			stand = ignore,
+		},
+	}
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	M.players[player:get_player_name()] = nil
+end)
---
+++ mods/magic/aurum_portals/init.lua
@@ -0,0 +1,128 @@
+local S = minetest.get_translator()
+aurum.portals = {}
+
+function aurum.portals.relative_pos(from, to, pos)
+	local from = aurum.realms.get(from)
+	local to = aurum.realms.get(to)
+
+	return aurum.gpos(to.id, vector.round(vector.multiply(aurum.rpos(from.id, pos), vector.divide(to.size, from.size))))
+end
+
+local base_def = {
+	description = S"Portal Base",
+	-- The flavor dust vanishes into another realm.
+	_doc_items_longdesc = S"A slab of golden power. It is perfectly clean; dust vanishes upon touching it.",
+	tiles = {"aurum_portals_base.png"},
+
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, -0.1, 0.5},
+		},
+	},
+
+	paramtype = "light",
+	light_source = 3,
+	sunlight_propagates = true,
+
+	groups = {dig_pick = 2, level = 2},
+	sounds = aurum.sounds.metal(),
+
+	drop = "aurum_portals:base",
+}
+minetest.register_node("aurum_portals:base", base_def)
+
+minetest.register_craft{
+	output = "aurum_portals:base",
+	recipe = {
+		{"aurum_ore:gold_ingot", "aurum_ore:gold_ingot", "aurum_ore:gold_ingot"},
+		{"aurum_ore:iron_ingot", "aurum_ore:mana_bean", "aurum_ore:iron_ingot"},
+		{"aurum_ore:gold_ingot", "aurum_ore:gold_ingot", "aurum_ore:gold_ingot"},
+	},
+}
+
+local teleporting = {}
+
+function aurum.portals.teleport(player, from_pos, to_realm)
+	local from_realm = aurum.pos_to_realm(from_pos)
+	if not from_realm then
+		minetest.log("warning", ("Invalid portal teleportation attempted at %s to %s by %s."):format(minetest.pos_to_string(from_pos), to_realm, player:get_player_name()))
+		return
+	end
+	local rdef = aurum.realms.get(to_realm)
+
+	local name = player:get_player_name()
+	local key = ("%d %d %s"):format(
+		minetest.hash_node_position(from_pos),
+		minetest.hash_node_position(gnode_augment.players[name].nodes.stand_pos),
+		to_realm
+	)
+
+	if teleporting[name] == key then
+		return
+	end
+
+	if teleporting[name] then
+		aurum.info_message(player, S"Teleportation cancelled.")
+	end
+
+	aurum.info_message(player, S("Teleporting to @1...", rdef.description))
+	teleporting[name] = key
+
+	local function landing_point(create)
+		local pos = aurum.portals.relative_pos(from_realm, to_realm, from_pos)
+		return pos
+	end
+
+	aurum.player.teleport_guarantee(player, aurum.box.new_radius(landing_point(), vector.new(32, 32, 32)), function(player)
+		aurum.player.teleport(player, landing_point(true))
+		aurum.info_message(player, S("Teleported to @1.", rdef.description))
+		teleporting[name] = nil
+	end, function(player)
+		-- Recalculate key.
+		local key = ("%d %d %s"):format(
+			minetest.hash_node_position(from_pos),
+			minetest.hash_node_position(gnode_augment.players[name].nodes.stand_pos),
+			to_realm
+		)
+
+		if teleporting[name] ~= key then
+			aurum.info_message(player, S"Teleportation cancelled.")
+			return true
+		end
+
+		return false
+	end)
+end
+
+minetest.register_on_leaveplayer(function(player)
+	teleporting[player:get_player_name()] = nil
+end)
+
+function aurum.portals.register(realm, def)
+	local rdef = aurum.realms.get(realm)
+
+	local def = table.combine({
+		color = "#FFFFFF",
+		name = "aurum_portals:portal_" .. realm,
+	}, def)
+
+	def.node = table.combine(base_def, {
+		description = S("Portal to @1", rdef.description),
+		_doc_items_longdesc = S"The gateway to another realm.",
+		_doc_items_usagehelp = S"Stand on the portal to be transported.",
+		light_source = 13,
+		tiles = {"aurum_portals_base.png^(aurum_portals_active.png^[colorize:" .. def.color .. ":127)"},
+
+		_on_standing = function(pos, node, obj)
+			if obj:is_player() then
+				aurum.portals.teleport(obj, pos, realm)
+			end
+		end,
+	}, def.node or {})
+
+	minetest.register_node(":" .. def.name, def.node)
+end
+
+aurum.dofile("default_portals.lua")
