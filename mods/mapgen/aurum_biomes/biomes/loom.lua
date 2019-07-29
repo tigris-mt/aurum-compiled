6ae62e7748e64f41059300c83458dee25833bd19
Beha <shacknetisp@mail.com>
2019-07-29 19:28:22 +0000

    Update: aurum master at e49876a

--- mods/mapgen/aurum_biomes/biomes/loom.lua
+++ mods/mapgen/aurum_biomes/biomes/loom.lua
@@ -8,11 +8,48 @@ end
 aurum.biomes.register_all("aurum:loom", {
 	name = "loom_barrens",
 	_groups = {"barren"},
-	heat_point = 30,
-	humidity_point = 30,
+	heat_point = 80,
+	humidity_point = 20,
 	_variants = {
 		base = aurum.biomes.v_base{},
 		ocean = v_ocean{},
 		under = aurum.biomes.v_under{},
 	},
 })
+
+aurum.biomes.register_all("aurum:loom", {
+	name = "loom_forest",
+	_groups = {"forest"},
+	heat_point = 50,
+	humidity_point = 50,
+	_variants = {
+		base = aurum.biomes.v_base{
+			node_top = "aurum_base:dirt",
+			depth_top = 2,
+		},
+		ocean = v_ocean{},
+		under = aurum.biomes.v_under{},
+	},
+})
+
+aurum.biomes.register_tree_decoration({
+	name = "aurum_trees:drywood",
+	biomes = {"loom_forest"},
+})
+
+minetest.register_decoration{
+	name = "loom_fire",
+	decoration = "fire:permanent_flame",
+	deco_type = "simple",
+	place_on = {"aurum_base:regret"},
+	sidelen = 16,
+	noise_params = {
+		offset = 0,
+		scale = 0.05,
+		spread = vector.new(200, 200, 200),
+		seed = 99,
+		octaves = 3,
+		persist = 0.5,
+	},
+	biomes = aurum.biomes.get_all_group("aurum:loom"),
+}
