9812d92769ccee2f90a7340f98f69319dfddde3b
Beha <shacknetisp@mail.com>
2020-03-24 12:33:33 +0000

    Update: aurum master at af83cd7

--- mods/mapgen/aurum_biomes/biomes/aether.lua
+++ mods/mapgen/aurum_biomes/biomes/aether.lua
@@ -9,15 +9,12 @@ end
 
 local v_base = v_n("v_base", {
 	y_min = 4,
-	node_top = "aurum_base:aether_shell",
-		depth_top = 1,
-		node_filler = "aurum_base:aether_skin",
-		depth_filler = 3,
-	node_stone = "aurum_base:aether_flesh",
+	depth_top = 2,
+	depth_filler = 2,
 })
 
 local v_under = v_n("v_under", {
-	y_max = 4,
+	y_max = 3,
 	node_stone = "aurum_base:aether_source",
 })
 
@@ -31,6 +28,12 @@ aurum.biomes.register_all("aurum:aether", {
 	},
 })
 
+aurum.biomes.trees.register({
+	name = "aurum_trees:aether_flesh",
+	biomes = {"aether_living"},
+	rarity = 0.1,
+})
+
 aurum.biomes.register_all("aurum:aether", {
 	name = "aether_white_crystal_forest",
 	heat_point = 70,
@@ -41,17 +44,11 @@ aurum.biomes.register_all("aurum:aether", {
 	},
 })
 
-aurum.biomes.trees.register({
+aurum.biomes.trees.register(b.t.combine({
 	name = "aurum_trees:white_crystal",
 	biomes = {"aether_white_crystal_forest"},
 	rarity = 1,
-	custom_schematics = {
-		["tree,24,16"] = 0.1,
-		["tree,32,14"] = 0.1,
-		["tree,16,22"] = 0.1,
-		["cone,12"] = 0.1,
-	},
-})
+}, aurum.trees.HUGE))
 
 aurum.biomes.trees.register({
 	name = "aurum_trees:yellow_crystal",
