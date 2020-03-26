3ce33c5e164317bf40acfad588cc5bccf8a9c3a0
Beha <shacknetisp@mail.com>
2020-03-26 20:50:31 +0000

    Update: aurum master at d3ee215

---
+++ mods/env/aurum_caves/decorations/vines.lua
@@ -0,0 +1,121 @@
+local S = minetest.get_translator()
+local MAX_SEARCH_HEIGHT = 160
+
+for _,c in ipairs{
+	{
+		name = "red",
+		desc = "Red",
+		texture = "[colorize:#FF0000:127",
+	},
+	{
+		name = "yellow",
+		desc = "Yellow",
+		texture = "[colorize:#FFFF00:127",
+	},
+	{
+		name = "white",
+		desc = "White",
+		texture = "[colorize:#FFFFFF:127",
+	},
+	{
+		name = "blue",
+		desc = "Blue",
+		texture = "[colorize:#0000FF:127",
+	},
+} do
+	aurum.flora.register("aurum_caves:vine_" .. c.name, {
+		description = S(c.desc .. " Cave Vine"),
+		_doc_items_longdesc = S"A glowing plant that descends from cave ceilings.",
+		tiles = {"aurum_caves_vine.png^" .. c.texture},
+		groups = {flora = 0, attached_node = 0},
+		waving = 0,
+		buildable_to = false,
+		selection_box = {
+			type = "fixed",
+			fixed = {
+				-0.35, -0.5, -0.35,
+				0.35, 0.5, 0.35,
+			},
+		},
+
+		climbable = true,
+
+		light_source = 9,
+
+		-- Recursively dig down.
+		after_dig_node = function(pos, node, _, digger)
+			local below = vector.subtract(pos, vector.new(0, 1, 0))
+			local bnode = minetest.get_node(below)
+			if bnode.name == node.name or bnode.name == "aurum_caves:vine_fruit" then
+				minetest.dig_node(below, bnode, digger)
+			end
+		end,
+	})
+end
+
+minetest.register_node("aurum_caves:vine_fruit", {
+	description = S"Cave Vine Fruit",
+	_doc_items_longdesc = S"The juicy fruit of the cave vine.",
+	sounds = aurum.sounds.wood(),
+	tiles = {"aurum_caves_vine_fruit.png"},
+	paramtype = "light",
+	light_source = 12,
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			-0.35, -0.5, -0.35,
+			0.35, 0.5, 0.35,
+		},
+	},
+	groups = {dig_chop = 3, edible = 4},
+	on_use = minetest.item_eat(4),
+})
+
+aurum.features.register_decoration{
+	place_on = {
+		"aurum_base:stone",
+		"aurum_base:regret",
+	},
+	rarity = 0.01,
+	biomes = aurum.biomes.get_all_group("all", {"under"}),
+
+	on_offset = function(c)
+		for i=1,MAX_SEARCH_HEIGHT do
+			local pos = vector.add(c.pos, vector.new(0, i, 0))
+			local nn = aurum.force_get_node(pos).name
+			if nn ~= "air" then
+				if nn == "ignore" then
+					return nil
+				else
+					c.s.height = c.random(i - 1)
+					pos.y = pos.y - c.s.height
+					return pos
+				end
+			end
+		end
+	end,
+
+	make_schematic = function(c)
+		local biome = b.t.combine({heat = 50, humidity = 50}, minetest.get_biome_data(c.pos) or {})
+		local node
+		if biome.heat < 25 then
+			node = "aurum_caves:vine_white"
+		elseif biome.heat < 50 then
+			node = "aurum_caves:vine_blue"
+		elseif biome.heat < 75 then
+			node = "aurum_caves:vine_yellow"
+		else
+			node = "aurum_caves:vine_red"
+		end
+		local size = vector.new(1, c.s.height, 1)
+		local data = {}
+		for i=1,size.y do
+			table.insert(data, {{node}})
+		end
+		if size.y > 1 and c.random() < 0.25 then
+			data[size.y] = {{"aurum_caves:vine_fruit"}}
+		end
+		return aurum.features.schematic(size, data)
+	end,
+}
