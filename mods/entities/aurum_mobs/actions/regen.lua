6efe9c043fa8007cfe4c9d65421d16791feb2573
Beha <shacknetisp@mail.com>
2020-04-18 04:13:42 +0000

    Update: aurum master at 93695c2

--- mods/entities/aurum_mobs/actions/regen.lua
+++ mods/entities/aurum_mobs/actions/regen.lua
@@ -1,4 +1,4 @@
-aurum.mobs.initial_data.regen_rate = 1 / 10
+aurum.mobs.initial_data.regen_rate = 1 / 5
 
 gemai.register_action("aurum_mobs:regen", function(self)
 	self.entity.object:set_hp(math.min(self.entity.object:get_properties().hp_max, self.entity.object:get_hp() + b.random_whole(self.data.step_time * self.data.regen_rate)))
