43f106abf611c5caa1a0a4462bbe8c2cfe9b6f3a
Beha <shacknetisp@mail.com>
2020-10-22 23:57:16 +0000

    Update: aurum master at a2f1037

--- tools/release.sh
+++ tools/release.sh
@@ -8,7 +8,7 @@ if ! [ "$(git rev-parse --abbrev-ref HEAD)" == "master" ]; then
 	exit 1
 fi
 
-if [ != "$1" "commit" ]; then
+if [ "$1" != "commit" ]; then
 	if ! git diff-index --quiet HEAD --; then
 		echo "Uncommitted changes, cannot procede."
 		exit 1
@@ -18,7 +18,7 @@ fi
 echo "Updating..."
 tools/make_readme.sh
 git add .
-git commit -m "Release update: $(version)"
+git commit -m "Release update: $(version)" || echo "No changes in release..."
 
 echo "Tagging..."
 tools/tag.sh
